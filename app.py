adcebe2901ea247d58fd6a3440316c96e3ef7265
samrus98 <samrus98@users.noreply.github.com>
2022-02-23 17:28:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+beers=['Hoegaarden', 'Sierra Nevada', 'Blue Moon', 'Kingfisher Ultra']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+color1='lightorange'
+color2='yellow'
+mytitle="Comparison"
 tabtitle='beer!'
-myheading='Flying Dog Beers'
+myheading="Sam's favorite beers"
 label1='IBU'
 label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+githublink='https://github.com/samrus98/102-flying-dog-beers.git'
+#sourceurl='https://www.flyingdog.com/beers/'
 
 ########### Set up the chart
 bitterness = go.Bar(
